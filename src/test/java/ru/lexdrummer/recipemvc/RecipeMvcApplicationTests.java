790989dd91e752d26f3420f4364752e15826989f
Alexandr Zarubin <lex.koenig.drummer@gmail.com>
2021-06-09 12:00:33 +0200

    initializing project

---
+++ src/test/java/ru/lexdrummer/recipemvc/RecipeMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lexdrummer.recipemvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
