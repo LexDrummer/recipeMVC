43451b1006972c3d3dfcafc5980575b22f653027
Alexandr Zarubin <lex.koenig.drummer@gmail.com>
2021-06-13 14:49:13 +0200

    add Category class and many to many rel to recipe

--- src/main/java/ru/lexdrummer/recipemvc/domain/Recipe.java
+++ src/main/java/ru/lexdrummer/recipemvc/domain/Recipe.java
@@ -24,6 +24,11 @@ public class Recipe {
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
 
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+        joinColumns = @JoinColumn(name = "recipe_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
+
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
 
@@ -109,6 +114,14 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 
     public Set<Ingredient> getIngredients() {
