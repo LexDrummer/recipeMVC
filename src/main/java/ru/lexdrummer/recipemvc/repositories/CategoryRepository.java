881dcd3bea4393b81b86b4fb1c71abc14180928a
Alexandr Zarubin <lex.koenig.drummer@gmail.com>
2021-06-15 14:34:33 +0200

    add data.sql, upgrade application.properties

--- src/main/java/ru/lexdrummer/recipemvc/repositories/CategoryRepository.java
+++ src/main/java/ru/lexdrummer/recipemvc/repositories/CategoryRepository.java
@@ -3,6 +3,9 @@ package ru.lexdrummer.recipemvc.repositories;
 import org.springframework.data.repository.CrudRepository;
 import ru.lexdrummer.recipemvc.domain.Category;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
 
+    Optional<Category> findByDescription(String description);
 }
--- src/main/java/ru/lexdrummer/recipemvc/repositories/UnitOfMeasureRepository.java
+++ src/main/java/ru/lexdrummer/recipemvc/repositories/UnitOfMeasureRepository.java
@@ -3,6 +3,9 @@ package ru.lexdrummer.recipemvc.repositories;
 import org.springframework.data.repository.CrudRepository;
 import ru.lexdrummer.recipemvc.domain.UnitOfMeasure;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
 
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
